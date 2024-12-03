8de592b3381e1b45665611fb221796b7a61c5a1a
Yash <ymalegao@ucsc.edu>
2024-12-02 19:47:18 -0800

    fog added needs to be fixed for camera

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -3,37 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-
 public class StateManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public TextMeshProUGUI BeersDrunkText; //
-    public int BeersDrunk = 0;
+    // UI Elements
+    public TextMeshProUGUI BeersDrunkText; 
+    public TextMeshProUGUI LivesText;
 
-    public TextMeshProUGUI LivesText; //
+    // Player Stats
+    public int BeersDrunk = 0;
     public int Lives = 3;
+
+    // Fog Variables
+    [Header("Fog Settings")]
+    public float initialFogDensity = 0.01f; // Starting fog density
+    public float maxFogDensity = 0.3f;      // Maximum fog density
+    public float fogIncreaseRate = 0.01f;   // Increase rate per beer
+    public Color fogColor = new Color(1f, 0.65f, 0.2f); // Yellowish-orange fog color
 
     void Start()
     {
+        // Initialize UI
         BeersDrunkText.text = "Beers Drunk: " + BeersDrunk.ToString();
-
         LivesText.text = "Lives: " + Lives.ToString();
-        
+
+        // Initialize Fog
+        RenderSettings.fog = true;
+        RenderSettings.fogColor = fogColor;
+        RenderSettings.fogDensity = initialFogDensity;
     }
 
-    // Update is called once per frame
     void Update()
     {
-
+        // Update UI
         BeersDrunkText.text = "Beers Drunk: " + BeersDrunk.ToString();
         LivesText.text = "Lives: " + Lives.ToString();
-
-
     }
 
     public void AddBeer()
     {
         BeersDrunk++;
+
+        // Adjust fog density dynamically
+        float targetDensity = initialFogDensity + (BeersDrunk * fogIncreaseRate);
+        RenderSettings.fogDensity = Mathf.Clamp(targetDensity, initialFogDensity, maxFogDensity);
     }
 
     public void LoseLife()
